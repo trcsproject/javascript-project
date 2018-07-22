125064327b32b34eb69784c4ea25c08bd882e9f5
Tiago R - Way <p000tiagorasilva@prservicos.com.br>
2018-07-22 14:58:49 -0300

    adicionado novo recurso

---
+++ replace-number-for-char/script.js
@@ -0,0 +1,25 @@
+function replaceStringForNumber(str){
+	for(var x = 0; x <= str.length; x++){
+		for(var y = 0; y < 10; y++){
+			str = str.replace(y,"");
+		}
+	}
+	return str;
+}
+//Execução
+document.write(replaceStringForNumber("Na1r22uto v34en556ce98u Sa09832suke!"));
+document.write("<br>");
+
+function replaceNumberForString(str){
+
+	var listChar = ['A', 'E', 'I', 'O', 'U', 'Q', 'K', 'R', 'T', 'P'];
+
+	for(var x = 0; x <= str.length; x++){
+		for(var y = 0; y <= listChar.length; y++){
+			str = str.replace(y,listChar[y]);
+		}
+	}
+	return str;
+}
+//Execução
+document.write(replaceNumberForString("N0r4t3 v1nc14 S0s4k1!"));
